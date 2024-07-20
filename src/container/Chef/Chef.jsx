271f2ce35f5bce4085021fde8e90d35e671bb590
yunusemreaydinn <ea.emreaydn.58@gmail.com>
2024-07-20 18:26:01 +0300

    v1.1

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">
+            Our dishes are crafted with fresh, local ingredients,
+          </p>
+        </div>
+        <p className="p__opensans">
+          {" "}
+          and you'll find our passion in every plate. We strive to bring you
+          only the best every day.{" "}
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
